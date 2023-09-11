080ec83e7be784120886aadae01901c7aa8c81c6
Colin Mothersead <cmothersead@gmail.com>
2023-09-11 11:36:42 -0400

    Retry export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,19 +1,21 @@
 export const InputType = {
-	color: 'color',
-	date: 'date',
-	'datetime-local': 'datetime-local',
-	email: 'email',
-	file: 'file',
-	month: 'month',
-	number: 'number',
-	password: 'password',
-	range: 'range',
-	search: 'search',
-	tel: 'tel',
-	text: 'text',
-	time: 'time',
-	typeahead: 'typeahead',
-	url: 'url'
+  color: 'color',
+  date: 'date',
+  'datetime-local': 'datetime-local',
+  email: 'email',
+  file: 'file',
+  month: 'month',
+  number: 'number',
+  password: 'password',
+  range: 'range',
+  search: 'search',
+  tel: 'tel',
+  text: 'text',
+  time: 'time',
+  typeahead: 'typeahead',
+  url: 'url'
 } as const;
 
 export type Item = { value: any; label: string };
+
+export { default as ChevronIcon } from './ChevronIcon.svelte';
