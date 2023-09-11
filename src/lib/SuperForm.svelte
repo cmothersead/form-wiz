135d15981be059ca87225e6310c015f0662b65ee
Colin Mothersead <cmothersead@gmail.com>
2023-09-11 12:32:02 -0400

    Import full instead of type

--- src/lib/SuperForm.svelte
+++ src/lib/SuperForm.svelte
@@ -2,7 +2,8 @@
   import type { FormPathLeaves, ZodValidation } from "sveltekit-superforms";
   import type { SuperForm } from "sveltekit-superforms/client";
   import type { AnyZodObject, z } from "zod";
-  import type { InputType, Item, LabeledIcon, SuperInput } from "$lib";
+  import type { InputType, Item } from "$lib";
+  import { LabeledIcon, SuperInput } from '$lib';
 
   type T = $$Generic<AnyZodObject>;
   type Form = SuperForm<ZodValidation<T>, unknown>;
